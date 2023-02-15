d866964d6f07e2a23416de836b40235def4bccde
Christopher Kim <christopher.kim@originate.com>
2023-02-14 17:14:53 -0800

    Fix terraform_remote_state bug with first time run

--- terraform/addons/environment/remote_state.tf
+++ terraform/addons/environment/remote_state.tf
@@ -1,3 +1,11 @@
+locals {
+  # Provides outputs from the previous successful run
+  previous_outputs = try(data.terraform_remote_state.self[0].outputs, {})
+
+  tfstate_workspace_prefix = "environment"
+  tfstate_key              = "terraform.tfstate"
+}
+
 data "terraform_remote_state" "backend" {
   backend = "local"
 
@@ -6,8 +14,14 @@ data "terraform_remote_state" "backend" {
   }
 }
 
-# Provides outputs from the previous successful run
+data "aws_s3_objects" "tfstate" {
+  bucket = data.terraform_remote_state.backend.outputs.bucket_name
+  prefix = terraform.workspace == "default" ? local.tfstate_key : "${local.tfstate_workspace_prefix}/${terraform.workspace}/${local.tfstate_key}"
+}
+
 data "terraform_remote_state" "self" {
+  count = length(data.aws_s3_objects.tfstate.keys) > 0 ? 1 : 0
+
   backend   = "s3"
   workspace = terraform.workspace
 
@@ -16,7 +30,7 @@ data "terraform_remote_state" "self" {
     region  = data.terraform_remote_state.backend.outputs.region
     bucket  = data.terraform_remote_state.backend.outputs.bucket_name
 
-    workspace_key_prefix = "environment"
-    key                  = "terraform.tfstate"
+    workspace_key_prefix = local.tfstate_workspace_prefix
+    key                  = local.tfstate_key
   }
 }
